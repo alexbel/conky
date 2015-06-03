290f105b7a80cd70555ff27ef8aa5ca29d4c2500
Alexander Belyaev <alexbel@lavabit.com>
2015-06-03 19:20:00 -0400

    Change service for getting public IP address

--- scripts/network_general_info.rb
+++ scripts/network_general_info.rb
@@ -1,4 +1,4 @@
-output = "${color0}Public IP: ${color}${execi 3600 curl -s http://checkip.dyndns.org/ | awk '/:/ {print $6}' | cut -d \"<\" -f1 }\n"
+output = ''
 output << "${color0}Gateway IP: $color$gw_ip\n"
 output << "${color0}DNS: $color$nameserver\n"
 output << "${color0}Wi-fi Network: $color${wireless_essid wlp3s0}\n"
