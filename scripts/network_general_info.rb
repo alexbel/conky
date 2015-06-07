9384eb397647ddd9065f47bda20a967d04795425
Alexander Belyaev <alexbel@lavabit.com>
2015-06-07 10:27:33 -0400

    Add second nameserver

--- scripts/network_general_info.rb
+++ scripts/network_general_info.rb
@@ -1,6 +1,6 @@
 output = ''
 output << "${color0}Gateway IP: $color$gw_ip\n"
-output << "${color0}DNS: $color$nameserver\n"
+output << "${color0}DNS: $color$nameserver, ${nameserver 1}\n"
 output << "${color0}Wi-fi Network: $color${wireless_essid wlp3s0}\n"
 output << "${color0}Wi-fi Ip: $color${addr wlp3s0}\n"
 output << "${color0}Signal: $color${wireless_link_qual_perc wlp3s0}${goto 95}% ${goto 112}${if_up wlp3s0}${wireless_link_bar wlp3s0}${else}${color0}No wlan0$endif\n"
