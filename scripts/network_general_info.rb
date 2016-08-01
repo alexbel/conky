53583e2f4e1766fb577640105e861f043f2151f3
Alexander Belyaev <alexbel@lavabit.com>
2016-08-01 15:43:11 -0700

    Detect amount of DNS servers

--- scripts/network_general_info.rb
+++ scripts/network_general_info.rb
@@ -1,6 +1,8 @@
+nameservers = `cat /etc/resolv.conf | grep ^nameserver | awk '{print $2}'`.split("\n").join(',')
+
 output = ''
 output << "${color0}Gateway IP: $color$gw_ip\n"
-output << "${color0}DNS: $color$nameserver, ${nameserver 1}\n"
+output << "${color0}DNS: $color #{nameservers}\n"
 output << "${color0}Wi-fi Network: $color${wireless_essid wlp3s0}\n"
 output << "${color0}Wi-fi Ip: $color${addr wlp3s0}\n"
 output << "${color0}Signal: $color${wireless_link_qual_perc wlp3s0}${goto 95}% ${goto 112}${if_up wlp3s0}${wireless_link_bar wlp3s0}${else}${color0}No wlan0$endif\n"
