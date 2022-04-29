edf15bd7412dcc0cebf70edd942c4f2d095a36e6
Alexander Belyaev <alexbel@tutanota.com>
2022-04-29 11:16:21 -0700

    Update

--- scripts/hard_drives.rb
+++ scripts/hard_drives.rb
@@ -4,11 +4,11 @@ active_device = secrets['hard_drives']['activity_for']
 
 diskio = "${color}#{active_device}${goto 130}${diskio_write #{active_device}}${goto 240}${diskio_read #{active_device}}${diskiograph #{active_device} 12,160 467f77 303030 -t -l}\n"
 
-free_used_title = "$color0${voffset 5}${goto 100}Free${goto 200}Used${goto 300}Size${goto 420}Used"
+free_used_title = "$color0${voffset 5}${goto 100}Total${goto 200}Used${goto 300}Free${goto 420}Used"
 
 partitions = []
 secrets['hard_drives']['partitions'].each do |partition_name, partition|
-  conky_partition = "${color0}#{partition_name}$color${goto 90}${fs_free #{partition}}${goto 190}${fs_used #{partition}}${goto 290}${fs_size #{partition}}${goto 390}${fs_bar 14,100 #{partition}}${goto 430}$color0${fs_used_perc #{partition}}%"
+  conky_partition = "${color0}#{partition_name}$color${goto 90}${fs_size #{partition}}${goto 190}${fs_used #{partition}}${goto 290}${fs_free #{partition}}${goto 390}${fs_bar 14,100 #{partition}}${goto 430}$color0${fs_used_perc #{partition}}%"
   partitions << conky_partition + "\n"
 end
 
